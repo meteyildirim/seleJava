67bae4c3b8c55e40e41c585394d95882009f3d51
meteyildirim <meteyildirim2005@gmail.com>
2022-06-30 10:37:26 +0200

    second commit

---
+++ sel_java/SelIntroduction.java
@@ -0,0 +1,13 @@
+package sel_java;
+
+public class SelIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*".repeat(70)+"\n");
+		System.out.println("Welcome to Selenium Intro with Java\n");
+		System.out.println("*".repeat(70));
+
+	}
+
+}
